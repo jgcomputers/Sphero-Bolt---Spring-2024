0fd18f584d871df97d23fe29cc4121eea50a7959
Gibbs <jgcomputers132@gmail.com>
2024-04-18 14:16:36 -0400

    Added Hello World script from Wiki

--- Sandbox.js
+++ Sandbox.js
@@ -5,6 +5,10 @@
 // Program: Hello World - Sandbox
 // Program URL: https://edu.sphero.com/program/16700416/edit
 
-async function startProgram() {
-	// Write code here
+async function startProgram() { 
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
 }
