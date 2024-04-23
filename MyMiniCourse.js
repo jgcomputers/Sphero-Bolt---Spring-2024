46ca63f0eb01ef7ebbbb8b668a5ec0f02ae9c76f
Gibbs <jgcomputers132@gmail.com>
2024-04-23 13:37:37 -0400

    Updated MyMiniCourse Content

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+// Programmer: Jordan Gibbs
+// Team Member: Aren Gay
+// Bolt ID: B0D0
+// Date: 4.23.2024
+// Program: My Mini Course
+// Program URL: https://edu.sphero.com/program/16743346/edit
+
+async function startProgram() {
+	
+}
