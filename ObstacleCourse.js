4db9733eb2b74e94142a87466aee78a712087130
Gibbs <jgcomputers132@gmail.com>
2024-04-26 14:07:54 -0400

    Fourth Checkpoint

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -20,4 +20,13 @@ async function startProgram() {
 
 	await roll((getHeading() + 0), 50,1.2);
 	await delay(1);
+
+	//Aren CheckPoint 4 - PC 2 - Added color Change
+	setMainLed({ r: 0, g: 200, b: 30});
+	await roll((getHeading() + 270), 10, 2);
+	await delay(1);
+	await roll((getHeading() + 0), 60,1.4);
+	await delay(1);
+
 }
+
