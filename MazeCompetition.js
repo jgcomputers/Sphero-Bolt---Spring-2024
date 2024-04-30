182c5d064ec4b3018a50e59de3564e0af126ac12
Gibbs <jgcomputers132@gmail.com>
2024-04-30 13:28:25 -0400

    Added beginning comments and startProgram js

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Jordan Gibbs
+// Team Member: Aren Gay
+// Bolt ID: B0D0
+// Date: 4.30.2024
+// Program: Maze Competition
+// Program URL: https://edu.sphero.com/program/16788352/edit
+
+async function startProgram() {
+	// Write code here
+}
