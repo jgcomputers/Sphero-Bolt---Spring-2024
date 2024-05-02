da65e9ec10d2614587bb7de1386619d3acacbe5c
Gibbs <jgcomputers132@gmail.com>
2024-05-02 13:46:15 -0400

    Third sound commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -13,29 +13,27 @@ async function startProgram() {
 
 
 	//Programmer: Aren Gay  - Blue LED Checkpoint Commit - Date: 5.1.24
-	await roll((getHeading() + 0), 65, 2.4);
+	await roll((getHeading() + 0), 68, 2.2);
 	await delay(1);
 	setMainLed({ r: 0, g: 0, b: 255 });
 
-	//Programmer:              - Sound One Checkpoint Commit - Date: 5.1.24
-	await roll((getHeading() + 90), 70, 1.45);
+	//Programmer: Aren Gay    - Sound One Checkpoint Commit - Date: 5.1.24
+	await roll((getHeading() + 90), 70, 1.6);
 	await Sound.Animal.Alligator.play(true);
 
 
-	//Programmer: Jordan Gibbs - Red LED Checkpoint Commit - Date:
-	
+	//Programmer: Jordan Gibbs         - Red LED Checkpoint Commit - Date:
 
-
-	//Programmer:          - Sound Two Checkpoint Commit - Date:
+	//Programmer:          - Sound Two Checkpoint Commit - Date:uuu
 
 
 
 
-	//Programmer:          - Green LED Checkpoint Commit - Date:
+	//Programmer:          - Green LED Checkpoint Commit - Date:vggg
 
 
 
-	//Programmer:          - Purple LED & Sound Three Checkpoint Commit - Date:
+	//Programmer:          - Purple LED & Sound Three Checkpoint Commit - Date:gggg
 
 
 
